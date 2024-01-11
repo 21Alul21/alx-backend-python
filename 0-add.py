81f42e7d86b47baefeaa60155ad4637c9b9402b5
21Alul21 <augustine.alul@yahoo.com>
2024-01-11 23:15:17 +0100

    fixed pycodestyle error

---
+++ 0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""this module contains a type annoted
+function that takes two float arguments and
+returns thier sum as float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """function that takes two float
+    values and returns their sum as float
+    """
+
+    return a + b
