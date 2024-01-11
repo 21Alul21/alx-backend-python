bb0663266b82bb3a81315255ff2f19739009e9be
21Alul21 <augustine.alul@yahoo.com>
2024-01-11 23:21:08 +0100

    fixed pycodestyle error again

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """this module contains a type annoted
-function that takes two float arguments and 
+function that takes two float arguments and
 returns thier sum as float
 """
 
@@ -9,6 +9,5 @@ def add(a: float, b: float) -> float:
     """function that takes two float
     values and returns their sum as float
     """
-
 
     return a + b
